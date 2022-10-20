a4ed86687a8c48783b7972c67358f0c40d11120a
Leonid Meleshin <lm@devolt.one>
2022-10-20 17:38:01 +0300

    feat(Battery): use sample rate for battery level

--- firmware/components/max17048.cpp
+++ firmware/components/max17048.cpp
@@ -1,22 +1,37 @@
 #include "openhaptics.h"
 #include "components/max17048.h"
+#include "config/battery.h"
 
 // @see https://github.com/sparkfun/SparkFun_MAX1704x_Fuel_Gauge_Arduino_Library/blob/main/examples/Example1_Simple/Example1_Simple.ino
 
 void MAX1704_BatteryLevel::setup()
 {
     // Set up the MAX17043 LiPo fuel gauge:
-    if (this->gauge->begin() == false) // Connect to the MAX17043 using the default wire port
-    {
-        // Serial.println(F("MAX17043 not detected. Please check wiring. Freezing."));
-        while (1);
+    this->active = this->gauge->begin();
+
+    if (this->active) { 
+        // Quick start restarts the MAX17043 in hopes of getting a more accurate
+        // guess for the SOC.
+        this->gauge->quickStart();
+
+        // We can set an interrupt to alert when the battery SoC gets too low.
+        // We can alert at anywhere between 1% - 32%:
+        this->gauge->setThreshold(20); // Set alert threshold to 20%.
+    }
+}
+
+void MAX1704_BatteryLevel::loop()
+{
+    if (!this->active) {
+        return;
     }
 
-    // Quick start restarts the MAX17043 in hopes of getting a more accurate
-	// guess for the SOC.
-	this->gauge->quickStart();
+    auto now_ms = millis();
 
-	// We can set an interrupt to alert when the battery SoC gets too low.
-	// We can alert at anywhere between 1% - 32%:
-	this->gauge->setThreshold(20); // Set alert threshold to 20%.
+    if (now_ms - this->last_battery_sample >= BATTERY_SAMPLE_RATE) {
+        this->last_battery_sample = now_ms;
+
+        this->voltage = this->gauge->getVoltage();
+        this->level = this->gauge->getSOC();
+    }
 }
--- include/components/max17048.h
+++ include/components/max17048.h
@@ -7,6 +7,10 @@
 class MAX1704_BatteryLevel : public Component
 {
     protected:
+        bool active = false;
+        unsigned long last_battery_sample = 0;
+        float voltage = 0;
+        float level = 0;
         SFE_MAX1704X* gauge;
     public:
         MAX1704_BatteryLevel(SFE_MAX1704X &gauge) : gauge(&gauge) {};
