a480f022007b587545f8208aef8b0ad2a7da3854
Leonid Meleshin <lm@devolt.one>
2022-11-17 15:58:29 +0300

    fix(VestX16): correct  meaningful skip statement

--- firmware/modes/bhaptics/tactsuit_x16.cpp
+++ firmware/modes/bhaptics/tactsuit_x16.cpp
@@ -92,7 +92,7 @@ Point2D* indexesToPoints[40] = {
 uint8_t groups[16] = { 0, 1, 4, 5, 10, 11, 14, 15, 20, 21, 24, 25, 30, 31, 34, 35, };
 
 void vestMotorTransformer(std::string& value) {
-    uint8_t result[40];
+    uint8_t result[40];;
 
     // Unpack values
     for (auto i = 0; i < 20; i++) {
@@ -135,25 +135,15 @@ void vestMotorTransformer(std::string& value) {
     }
     Serial.println(" }");
 
-    // Test 1
-    // for (auto i = 0; i < 40; i++) {
-    //     outputData_t output_0;
-    //     output_0.point = *indexesToPoints[i];
-    //     output_0.intensity = map(result[i], 0, 15, 0, UINT16_MAX);
-    //     App.getOutput()->writeOutput(
-    //         (i < 10 || i >= 30) ? OUTPUT_PATH_CHEST_FRONT : OUTPUT_PATH_CHEST_BACK,
-    //         output_0
-    //     );
-    // }
-
-    // Test 2
     for (auto i = 0; i < 40; i++) {
         // take only meaningful values
-        if (i != BH_X16_O_F00 || i != BH_X16_O_F01 || i != BH_X16_O_F10 || i != BH_X16_O_F11 || i != BH_X16_O_F20 || i != BH_X16_O_F21 || i != BH_X16_O_F30 || i != BH_X16_O_F31
-            || i != BH_X16_O_B00 || i != BH_X16_O_B01 || i != BH_X16_O_B10 || i != BH_X16_O_B11 || i != BH_X16_O_B20 || i != BH_X16_O_B21 || i != BH_X16_O_B30 || i != BH_X16_O_B31
+        if (i != BH_X16_O_F00 && i != BH_X16_O_F01 && i != BH_X16_O_F10 && i != BH_X16_O_F11 && i != BH_X16_O_F20 && i != BH_X16_O_F21 && i != BH_X16_O_F30 && i != BH_X16_O_F31
+            && i != BH_X16_O_B00 && i != BH_X16_O_B01 && i != BH_X16_O_B10 && i != BH_X16_O_B11 && i != BH_X16_O_B20 && i != BH_X16_O_B21 && i != BH_X16_O_B30 && i != BH_X16_O_B31
         ) {
             continue;
         }
+
+        Serial.printf("%u, ", i);
 
         outputData_t output_0;
         output_0.point = *indexesToPoints[i];
@@ -163,6 +153,7 @@ void vestMotorTransformer(std::string& value) {
             output_0
         );
     }
+    Serial.printf("\n");
 }
 
 void setupMode() {
