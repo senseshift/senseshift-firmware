ed6bf8a1be8b4591652e1f1b112dd192ada82b94
Leonid Meleshin <lm@devolt.one>
2022-12-26 13:38:12 +0400

    feat(Battery): add MAX17048 SOC

---
+++ include/battery/max17048.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SparkFun_MAX1704x_Fuel_Gauge_Arduino_Library.h>
+
+#include "battery/abstract_battery.h"
+
+class MAX1704_BatteryLevel : public AbstractBattery
+{
+    private:
+        bool active = false;
+        SFE_MAX1704X* gauge;
+    protected:
+        uint8_t updateLevel() override;
+    public:
+        MAX1704_BatteryLevel(SFE_MAX1704X &gauge) : gauge(&gauge) {};
+        void setup() override;
+};
