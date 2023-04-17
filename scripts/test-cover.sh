9e86ef77c3daeae057bcf3d77c5dc0cb63958a35
Leonid Meleshin <hello@leon0399.ru>
2023-04-17 12:28:18 +0400

    ci(GitHub): collect initial coverage upon build

--- scripts/test-cover.sh
+++ scripts/test-cover.sh
@@ -5,13 +5,9 @@ build_dir=$1
 env=$2
 shift 2
 
-# Generate initial coverage report
-lcov -i -d $build_dir/$env/ -c -o lcov.info.initial
-
 # Run tests
 $@
 
 lcov -d $build_dir/$env/ -c -o lcov.info
-lcov --add-tracefile lcov.info.initial -a lcov.info -o lcov.info.merged
-lcov --remove lcov.info.merged '/usr/include/*' '*.pio/*' '*/tool-unity/*' '*/test/*' '*/MockArduino/*' -o lcov.info.cleaned
+lcov --remove lcov.info '/usr/include/*' '*.platformio/*' '*.pio/*' '*/tool-unity/*' '*/test/*' '*/MockArduino/*' -o lcov.info.cleaned
 genhtml -p $PWD -o build/coverage/ --demangle-cpp lcov.info.cleaned
