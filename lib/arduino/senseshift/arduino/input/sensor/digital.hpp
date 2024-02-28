bbd9c89694ebb838de977217e3bcf60441ae8d20
Leonid Meleshin <hello@leon0399.ru>
2024-02-28 22:37:31 +0400

    refactor(Arduino): add pin_mode option to DigitalSensor

--- lib/arduino/senseshift/arduino/input/sensor/digital.hpp
+++ lib/arduino/senseshift/arduino/input/sensor/digital.hpp
@@ -1,31 +1,26 @@
 #pragma once
 
-#include <senseshift/input/sensor/sensor.hpp>
+#include <cstdint>
 
 #include <Arduino.h>
 
+#include <senseshift/input/sensor/sensor.hpp>
+
 namespace SenseShift::Arduino::Input {
-    template<bool Invert = false>
     class DigitalSimpleSensor : public ::SenseShift::Input::IBinarySimpleSensor {
-        uint8_t pin_;
-
       public:
-        DigitalSimpleSensor(const uint8_t pin) : pin_(pin) {}
+        explicit DigitalSimpleSensor(
+          const std::uint8_t pin, const std::uint8_t mode = INPUT_PULLUP, const std::uint8_t inverted = LOW
+        ) :
+          pin_(pin), mode_(mode), inverted_(inverted){};
 
-        void init() override { pinMode(this->pin_, INPUT_PULLUP); };
+        void init() override { pinMode(this->pin_, this->mode_); };
 
-        [[nodiscard]] auto getValue() -> bool override;
+        [[nodiscard]] auto getValue() -> bool override { return digitalRead(this->pin_) == this->inverted_; }
+
+      private:
+        std::uint8_t pin_;
+        std::uint8_t mode_ = INPUT_PULLUP;
+        std::uint8_t inverted_ = LOW;
     };
-
-    template<>
-    [[nodiscard]] inline auto DigitalSimpleSensor<false>::getValue() -> bool
-    {
-        return digitalRead(this->pin_) == LOW;
-    }
-
-    template<>
-    [[nodiscard]] inline auto DigitalSimpleSensor<true>::getValue() -> bool
-    {
-        return digitalRead(this->pin_) == HIGH;
-    }
 } // namespace SenseShift::Arduino::Input
